fe6ad7e1ab32d4391d38a2ec01d766ee4e7aec6b
rshyong <rshyong@gmail.com>
2018-08-25 13:08:56 -0400

    adding getProduct

--- contracts/OnlineMarketplace.sol
+++ contracts/OnlineMarketplace.sol
@@ -17,8 +17,10 @@ contract OnlineMarketplace {
 
   /** @dev Product struct. Contains price and quantity of the product. */
   struct Product{
+    string name;
     uint price;
     uint quantity;
+    string ipfsHash;
   }
 
   /** @dev Storefront struct. Contains name of store, image (stored as ipfs hash), funds, and a mapping to its products. */
@@ -26,6 +28,7 @@ contract OnlineMarketplace {
     string name;
     string ipfsHash;
     uint funds;
+    uint numProducts;
     mapping(string => Product) products;
   }
 
@@ -46,6 +49,12 @@ contract OnlineMarketplace {
 
   /** @dev Emit when a storefront is deleted */
   event EventDeleteStoreFront(string indexed _storeName);
+ 
+  /** @dev Emit when a product is added */
+  event EventAddProduct(string indexed _productName);
+
+  /** @dev Emit when a product is deleted */
+  event EventDeleteProduct(string indexed _productName);
 
   /** @dev Modifier that checks to make sure the sender is an admin. */
   modifier checkAdmin() {
@@ -67,7 +76,7 @@ contract OnlineMarketplace {
     _;
   }
 
-  /** @dev Modifier that checks to make storefront name is unique. 
+  /** @dev Modifier that checks storefront name is unique. 
   * @param _name Storefront name to be checked.
   */
   modifier checkStoreFrontName(string _name) {
@@ -79,6 +88,20 @@ contract OnlineMarketplace {
       }
     }
     require(checkName);
+    _;
+  }
+
+  /** @dev Modifier that checks storefront exists. 
+  * @param _index Index of storefront to be checked.
+  */
+  modifier checkStoreFrontExists(uint _index) {
+    require(keccak256(storeFronts[msg.sender][_index].name) != keccak256(''));
+    _;
+  }
+
+  /** @dev Modifier that checks for integer underflow. */
+  modifier checkIntegerUnderflow(uint _num) {
+    require(_num - 1 < _num);
     _;
   }
 
@@ -115,19 +138,65 @@ contract OnlineMarketplace {
   */
   function addStoreFront(string _name, string _ipfsHash) public checkOwner checkStoreFrontName(_name) {
     emit EventNewStoreFront(_name);
-    storeFronts[msg.sender].push(StoreFront(_name, _ipfsHash, 0));
+    storeFronts[msg.sender].push(StoreFront(_name, _ipfsHash, 0, 0));
     numStoreFronts[msg.sender]++;
   }
 
   /** @dev Delete a storefront.
   * @param _index Index of storefront to be deleted.
   */
-  function deleteStoreFront(uint _index) public checkOwner {
-    require(keccak256(storeFronts[msg.sender][_index].name) != keccak256('') && numStoreFronts[msg.sender] - 1 < numStoreFronts[msg.sender]);
+  function deleteStoreFront(uint _index) public checkOwner checkStoreFrontExists(_index) checkIntegerUnderflow(numStoreFronts[msg.sender]) {
     string _name = storeFronts[msg.sender][_index].name;
     emit EventDeleteStoreFront(_name);
     delete storeFronts[msg.sender][_index];
     numStoreFronts[msg.sender]--;
+  }
+
+  /** @dev Converts uint to string.
+  * @param i Integer to be converted to string.
+  * @return Returns string.
+  */
+  function uint2str(uint i) internal pure returns (string){
+    if (i == 0) return "0";
+    uint j = i;
+    uint length;
+    while (j != 0){
+        length++;
+        j /= 10;
+    }
+    bytes memory bstr = new bytes(length);
+    uint k = length - 1;
+    while (i != 0){
+        bstr[k--] = byte(48 + i % 10);
+        i /= 10;
+    }
+    return string(bstr);
+  }
+
+  /** @dev Add a product to storefront.
+  * @param _index Index of storefront where product is to be sold.
+  * @param _name Name of product.
+  * @param _price Price of product.
+  * @param _quantity Quantity of product to be sold.
+  */
+  function addProduct(uint _index, string _name, uint _price, uint _quantity, string _ipfsHash) public checkOwner checkStoreFrontExists(_index) {
+    require(_price > 0 && _quantity > 0);
+    emit EventAddProduct(_name);
+    uint numProducts = storeFronts[msg.sender][_index].numProducts;
+    string memory num = uint2str(numProducts);
+    storeFronts[msg.sender][_index].products[num].name = _name;
+    storeFronts[msg.sender][_index].products[num].price = _price;
+    storeFronts[msg.sender][_index].products[num].quantity = _quantity;
+    storeFronts[msg.sender][_index].products[num].ipfsHash = _ipfsHash;
+    storeFronts[msg.sender][_index].numProducts++;
+  }
+
+  /** @dev Get product from storefront.
+  * @param _index Index of storefront where product is to be sold.
+  * @param _num Index of product.
+  */
+  function getProduct(uint _index, string _num) public view returns (string, uint, uint) {
+    return (storeFronts[msg.sender][_index].products[_num].name, storeFronts[msg.sender][_index].products[_num].price, storeFronts[msg.sender][_index].products[_num].quantity);
   }
 
   /** @dev Fallback function. */
