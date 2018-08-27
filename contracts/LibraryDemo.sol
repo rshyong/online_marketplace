d9de9ddc4a37963592f8603050d69178280762de
rshyong <rshyong@gmail.com>
2018-08-27 19:49:06 -0400

    using safeMath library

---
+++ contracts/LibraryDemo.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.24;
+import './SafeMath.sol';
+
+/**
+ * @title Library Demo
+ * @dev Using SafeMath as a library
+ */
+ contract LibraryDemo {
+     using SafeMath for uint256;
+
+    /**@dev result of multiplication */
+     uint256 public mulResult;
+    
+    /**@dev result of division */
+     uint256 public divResult;
+    
+    /**@dev result of addition */
+     uint256 public addResult;
+    
+    /**@dev result of subtraction */
+     uint256 public subResult;
+    
+    /**@dev result of modulo */
+     uint256 public modResult;
+
+    /** Multiply two integers using SafeMath
+    *@param a Integer to be multiplied
+    *@param b Integer to be multiplied
+    */
+     function multiply(uint a, uint b) public {
+        mulResult = a.mul(b);
+     }
+
+    /** Divide two integers using SafeMath
+    *@param a Integer to be divided
+    *@param b Integer to be divided
+    */
+     function divide(uint a, uint b) public {
+        mulResult = a.div(b);
+     }
+
+    /** Add two integers using SafeMath
+    *@param a Integer to be added
+    *@param b Integer to be added
+    */
+     function add(uint a, uint b) public {
+        mulResult = a.add(b);
+     }
+
+    /** Subtract two integers using SafeMath
+    *@param a Integer to be subtracted
+    *@param b Integer to be subtracted
+    */
+     function subtract(uint a, uint b) public {
+        mulResult = a.sub(b);
+     }
+
+    /** Find the modulo of two integers using SafeMath
+    *@param a Integer
+    *@param b Integer
+    */
+     function modulo(uint a, uint b) public {
+        mulResult = a.mod(b);
+     }
+ }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var OnlineMarketplace = artifacts.require("./OnlineMarketplace.sol");
+const OnlineMarketplace = artifacts.require("./OnlineMarketplace.sol");
+const LibraryDemo = artifacts.require("./LibraryDemo.sol");
+const SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
   deployer.deploy(OnlineMarketplace);
+  deployer.link(SafeMath, LibraryDemo);
+  deployer.deploy(LibraryDemo);
 };
